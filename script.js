8b01b0a4d9dbcb79a6681dcea850965076c8bb81
hannalinda <nichelle.hanna@escola.pr.gov.br>
2026-09-10 10:10:19 -0300

    c

--- script.js
+++ script.js
@@ -22,7 +22,8 @@ const textoResultado = document.querySelector(".texto-resultado");
         enunciado: "A inteligência artificial pode realizar tarefas que antes eram feitas exclusivamente por pessoas. Como você acredita que devemos lidar com essa mudança no mercado de trabalho?",
         alternativas: [
             {
-                texto: "Investir na educação e preparar as pessoas para trabalhar junto com as novas tecnologias.",
+                texto: "Investir na educação e p
+reparar as pessoas para trabalhar junto com as novas tecnologias.",
                 afirmacao: "Você pensa no futuro e acredita que conhecimento e adaptação são fundamentais para enfrentar as mudanças tecnológicas."
             },
             {
@@ -88,3 +89,38 @@ const textoResultado = document.querySelector(".texto-resultado");
         ]
     }
 ];
+let atual = 0;
+let perguntaAtual;
+let historiaFinal = "";
+
+function mostraPergunta(){
+    if(atual >= perguntas.length){
+        mostraResultado();
+        return;
+    }
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
+    mostraAlternativas();
+}
+function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas){
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+
+}
+function respostaSelecionada(opcaoSelecionada) {
+    const afirmacoes = opcaoSelecionada.afirmacao;
+    historiaFinal += afirmacoes + " ";
+    atual++
+    mostraPergunta();
+}
+function mostraResultado(){
+    caixaPerguntas.textContent = "Olha só o que podemos afirmar sobre você...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
+}
+
